b25b7df79d1d084c484248f548876f220ec42b40
Maxime Grenier-Castillo <max.grenier99@gmail.com>
2019-01-25 23:49:58 -0500

    @@BUGGED@@Re-Added TileCollider and placed collision on all current map

---
+++ Smigeon/Assets/Script/Stats.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stas : MonoBehaviour
+{
+
+    private double attack;
+
+    public Stas() {
+
+        }
+
+}
---
+++ Smigeon/README.md
@@ -0,0 +1,1 @@
+# The-Smigeon-Game
