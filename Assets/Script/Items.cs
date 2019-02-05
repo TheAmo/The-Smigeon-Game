1943e2efb477888a9ac94959c99d92b177b0c687
Jacty <jactysaenz@gmail.com>
2019-02-05 12:29:03 -0500

    DB

---
+++ Assets/Script/Items.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityNpgsql;
+
+public class Items : MonoBehaviour
+{
+    string strConnection = "Server=localhost; Port=5432; DataBase=postgres; Username=postgres; Password=Milena14 ";
+
+    NpgsqlConnection dbConnection;
+    NpgsqlCommand dbCmd;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
