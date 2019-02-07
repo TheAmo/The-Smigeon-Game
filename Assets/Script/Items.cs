ce6778237dd381926fcfd9edcbb10c6eafae0cbd
Jacty <jactysaenz@gmail.com>
2019-02-07 18:08:14 -0500

    Fix DB

--- Assets/Script/Items.cs
+++ Assets/Script/Items.cs
@@ -7,13 +7,16 @@ public class Items : MonoBehaviour
 {
     string strConnection = "Server=localhost; Port=5432; DataBase=postgres; Username=postgres; Password=Milena14 ";
 
-    NpgsqlConnection dbConnection;
-    NpgsqlCommand dbCmd;
+    NpgsqlConnection dbConnection = null;
+    NpgsqlCommand dbCmd = null;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        dbConnection = new NpgsqlConnection(strConnection);
+
+
         
     }
 
