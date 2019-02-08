5721a5ad79f7cdc6be5adbf9ee60984170d19e22
Taha Mohamed Lazreg <tlazreg1@gmail.com>
2019-02-07 22:28:20 -0500

    ADD: Dialogue boxes and scripts
    Buttons still don't respond

---
+++ Assets/Script/Dialogue/DialogueManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text Name;
+    public Text Dialogue;
+    private Queue<string> sentences;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartText(Dialogue d)
+    {
+        Debug.Log("START DIALOGUE CALISS");
+        Name.text = d.name;
+        sentences.Clear();
+
+        foreach (string sentence in d.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextText();
+    }
+
+       
+    public void EndText()
+    {
+
+    }
+    public void DisplayNextText()
+    {
+         if(sentences.Count == 0)
+        {
+            EndText();
+            return;
+        }
+        string s = sentences.Dequeue();
+        Dialogue.text = s;
+    }
+
+}
---
+++ Assets/Script/Dialogue/Reference.cs
@@ -0,0 +1,1 @@
+﻿//Cette vidéo a été utilisée pour faire cette section: https://www.youtube.com/watch?v=_nRzoTzeyxU
