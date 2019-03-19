e386e85e548b5a698371e58a959a263f92ac7a77
Maxime Grenier <max.grenier99@gmail.com>
2019-03-19 11:00:37 -0400

    Started DB Implementation

--- Assets/Script/DataBase/DataBaseSmi.cs
+++ Assets/Script/DataBase/DataBaseSmi.cs
@@ -8,7 +8,6 @@ using UnityNpgsql;
 
 public class DataBaseSmi : MonoBehaviour
 {
-
     /*===================================================================================================================
     * Data Table
     * 
@@ -18,45 +17,55 @@ public class DataBaseSmi : MonoBehaviour
     private DataTable m_dbTableMaterial;
     private DataTable m_dbTableMaterialWeapon;
     private DataTable m_dbTableMonsterStats;
-
     private DataTable m_dbtmpTable;
+
+    private double m_val;
+    private double m_valMaterial;
 
     /*===================================================================================================================
     * Connection
     * 
     ===================================================================================================================*/
     private string strConnection = "Server=localhost; Port=5432; DataBase=dbsmigeon; Username=postgres; Password=Milena14";
-
     private NpgsqlConnection dbConnection = null;
     private NpgsqlCommand dbCmd = null;
+    private NpgsqlDataReader dbReader = null;
 
-    private DataTable receiveFromQuerry(string querry)
+    private DataTable Connection(string strQuery)
     {
+        m_dbTable = new DataTable();
         NpgsqlDataAdapter dbAdapter;
 
         dbConnection = new NpgsqlConnection(strConnection);
 
         dbConnection.Open();
-        dbCmd = new NpgsqlCommand(querry, dbConnection);
+        dbCmd = new NpgsqlCommand(strQuery, dbConnection);
         dbAdapter = new NpgsqlDataAdapter(dbCmd);
-        dbAdapter.Fill(m_dbtmpTable);
+        dbAdapter.Fill(m_dbTable);
         dbConnection.Close();
 
-        Debug.Log("Database connection succesful: " + querry);
+        return m_dbTable;
+    }
 
-        return (m_dbtmpTable);
+    private DataTable Select(string strSelect)
+    {
+        m_dbTable = new DataTable();
+
+        m_dbTable = Connection(strSelect);
+
+        return m_dbTable;
     }
 
     /*===================================================================================================================
-    *** =================================================================================================================
-    *** =================================================================================================================
-    *** Stats
-    *** 
-    ***
-    *** 
-    *** =================================================================================================================
-    *** =================================================================================================================
-    ===================================================================================================================*/
+ *** =================================================================================================================
+ *** =================================================================================================================
+ *** Stats
+ *** 
+ ***
+ *** 
+ *** =================================================================================================================
+ *** =================================================================================================================
+ ===================================================================================================================*/
 
     /*===================================================================================================================
      * Get Monster Stat by Id
@@ -65,12 +74,10 @@ public class DataBaseSmi : MonoBehaviour
     public DataTable getMonsterInfoById(int id)
     {
         m_dbTableMonsterStats = new DataTable();
-        m_dbTableMonsterStats = receiveFromQuerry("SELECT * FROM monster_stats WHERE id=" + id);
+        m_dbTableMonsterStats = Connection("SELECT * FROM monster_stats WHERE id=" + id);
+
         return m_dbTableMonsterStats;
     }
-
-
-
 
     /*===================================================================================================================
     *** =================================================================================================================
@@ -85,191 +92,137 @@ public class DataBaseSmi : MonoBehaviour
 
 
     /*===================================================================================================================
-    * Select Weapon
+    * Select Weapons and materials
     * 
     ===================================================================================================================*/
-    public DataTable SelectWeapons()
-    {
-         m_dbTableWeapon = new DataTable();
-        NpgsqlDataAdapter dbAdapter;
 
-        dbConnection = new NpgsqlConnection(strConnection);
-        dbConnection.Open();
-
-        string strSelect = "SELECT * FROM \"weapon\"";
-
-        dbCmd = new NpgsqlCommand(strSelect, dbConnection);
-
-        dbAdapter = new NpgsqlDataAdapter(dbCmd);
-
-        dbAdapter.Fill(m_dbTableWeapon);
-
-        dbConnection.Close();
-
-        return m_dbTableWeapon;
-    }
-    /*===================================================================================================================
-    * Select Material
-    * 
-    ===================================================================================================================*/
-    public DataTable SelectMaterials()
-    {
-        m_dbTableMaterial = new DataTable();
-        NpgsqlDataAdapter dbAdapter;
-
-        dbConnection = new NpgsqlConnection(strConnection);
-        dbConnection.Open();
-
-        string strSelect = "SELECT * FROM \"material\"";
-
-        dbCmd = new NpgsqlCommand(strSelect, dbConnection);
-
-        dbAdapter = new NpgsqlDataAdapter(dbCmd);
-
-        dbAdapter.Fill(m_dbTableMaterial);
-
-        dbConnection.Close();
-        
-        return m_dbTableMaterial;
-    }
-    /*===================================================================================================================
-    * Select weapon material
-    * 
-    ===================================================================================================================*/
     public DataTable SelectWeaponsMaterials()
     {
-        m_dbTableMaterialWeapon = new DataTable();
-        NpgsqlDataAdapter dbAdapter;
 
-        dbConnection = new NpgsqlConnection(strConnection);
-        dbConnection.Open();
+        m_dbTableMaterialWeapon = new DataTable();
 
         string strSelect = "SELECT * FROM \"weapon_material\"";
 
-        dbCmd = new NpgsqlCommand(strSelect, dbConnection);
-
-        dbAdapter = new NpgsqlDataAdapter(dbCmd);
-
-        dbAdapter.Fill(m_dbTableMaterialWeapon);
-
-        dbConnection.Close();
+        m_dbTableMaterialWeapon = Connection(strSelect);
 
         return m_dbTableMaterialWeapon;
-
     }
+
     /*===================================================================================================================
-    * Get Weapons
+    * Select Weapon
     * 
     ===================================================================================================================*/
-    public List<Items> getWeapons()
+    public List<Items> getAllWeapons()
     {
         List<Items> weapons = new List<Items>();
         string name;
-        int id, damage, defense, price;
+        int id, damage, defense;
+        double price;
 
-        DataTable m_dbTableWea = new DataTable();
-        m_dbTableWea = SelectWeapons();
+        string strSelect = "SELECT * FROM \"weapon\"";
 
-        for (int i = 0; i < m_dbTableWea.Rows.Count; i++)
+        m_dbTableWeapon = new DataTable();
+        m_dbTableWeapon = Select(strSelect);
+
+        for (int i = 0; i < m_dbTableWeapon.Rows.Count; i++)
         {
-            id = Convert.ToInt32(m_dbTableWea.Rows[i]["id"]);
-            name = (m_dbTableWea.Rows[i]["name"]).ToString();
-            damage = Convert.ToInt32(m_dbTableWea.Rows[i]["damage"]);
-            defense = Convert.ToInt32(m_dbTableWea.Rows[i]["defense"]);
-            price = Convert.ToInt32(m_dbTableWea.Rows[i]["price"]);
+            id = Convert.ToInt32(m_dbTableWeapon.Rows[i]["id"]);
+            name = (m_dbTableWeapon.Rows[i]["name"]).ToString();
+            damage = Convert.ToInt32(m_dbTableWeapon.Rows[i]["damage"]);
+            defense = Convert.ToInt32(m_dbTableWeapon.Rows[i]["defense"]);
+            price = Convert.ToDouble(m_dbTableWeapon.Rows[i]["price"]);
 
             weapons.Add(new Items(id, name, damage, defense, price));
         }
 
         return weapons;
     }
+
     /*===================================================================================================================
-    * Get Material
+    * Select Material
     * 
     ===================================================================================================================*/
-    public List<Items> getMaterials()
+    public List<Items> getAllMaterials()
     {
         List<Items> materials = new List<Items>();
         string name;
-        int id, damage, defense, price;
+        int id, damage, defense;
+        double price;
 
-        DataTable m_dbTableMa = new DataTable();
-        m_dbTableMa = SelectMaterials();
+        string strSelect = "SELECT * FROM \"material\"";
 
-        //Debug.Log(m_dbTableMa.Rows.Count);
-        for (int i = 0; i < m_dbTableMa.Rows.Count; i++)
+        m_dbTableMaterial = new DataTable();
+        m_dbTableMaterial = Select(strSelect);
+
+        for (int i = 0; i < m_dbTableMaterial.Rows.Count; i++)
         {
-            id = Convert.ToInt32(m_dbTableMa.Rows[i]["id"]);
-            name = (m_dbTableMa.Rows[i]["name"]).ToString();
-            damage = Convert.ToInt32(m_dbTableMa.Rows[i]["damage"]);
-            defense = Convert.ToInt32(m_dbTableMa.Rows[i]["defense"]);
-            price = Convert.ToInt32(m_dbTableMa.Rows[i]["price"]);
-
-            //Debug.Log(id + " " + name + " " + damage + " " + defense + " " + price);
+            id = Convert.ToInt32(m_dbTableMaterial.Rows[i]["id"]);
+            name = (m_dbTableMaterial.Rows[i]["name"]).ToString();
+            damage = Convert.ToInt32(m_dbTableMaterial.Rows[i]["damage"]);
+            defense = Convert.ToInt32(m_dbTableMaterial.Rows[i]["defense"]);
+            price = Convert.ToDouble(m_dbTableMaterial.Rows[i]["price"]);
 
             materials.Add(new Items(id, name, damage, defense, price));
-            
+
         }
 
         return materials;
     }
-    /*===================================================================================================================
-    * Get Material Weapon
-    * 
-    ===================================================================================================================*/
-    public List<Items> getMaterialWeapon()
+   /*===================================================================================================================
+   * Get Table value
+   * 
+   ===================================================================================================================*/
+    private double getTableValue(String strSelect)
     {
-        int id_weapon, id_material;
+        dbConnection = new NpgsqlConnection(strConnection);
+        dbConnection.Open();
 
-        List<Items> weapons = new List<Items>();
-        weapons = getWeapons();
-
-        List<Items> materials = new List<Items>();
-        materials = getMaterials();
-
-        List<Items> weapon_material = new List<Items>();
-
-        DataTable m_dbTableMaWea = new DataTable();
-        m_dbTableMaWea = SelectWeaponsMaterials();
-
-        for (int i = 0; i < m_dbTableMaWea.Rows.Count; i++)
+        using (dbCmd = new NpgsqlCommand(strSelect, dbConnection))
         {
-            id_weapon = Convert.ToInt32(m_dbTableMaWea.Rows[i]["id_weapon"]);
-            id_material = Convert.ToInt32(m_dbTableMaWea.Rows[i]["id_material"]);
-
-            for (int j = 0; j < weapons.Count; j++)
+            dbReader = dbCmd.ExecuteReader();
+            while (dbReader.Read())
             {
-                if (id_weapon == weapons[j].getId())
-                {
-                    Debug.Log("                    DANS IF ");
-                    weapon_material.Add(new Items(weapons[j].getName(),
-                        weapons[j].getDamage() + materials[id_material - 1].getDamage(),
-                        weapons[j].getDefense() + materials[id_material - 1].getDefense(),
-                        weapons[j].getPrice() + materials[id_material - 1].getPrice(),
-                        materials[id_material - 1].getName()));
-                    Debug.Log(weapon_material[j].getName() + " " + weapon_material[j].getDamage() + " " + weapon_material[j].getDefense() + " " + weapon_material[j].getPrice() + " " + weapon_material[j].getMaterial() + "                       ITEM ");
-                }
-                else Debug.Log(id_weapon + " != " + weapons[j].getId());
+                m_val = double.Parse(dbReader[0].ToString());
             }
         }
+        dbConnection.Close();
 
-        return weapon_material;
-        
+        return m_val;
+    }
+   /*===================================================================================================================
+   * Get Table info
+   * 
+   ===================================================================================================================*/
+    public double getTableInfo(String info, String name, String table, String material)
+    {
+        string strSelect = "SELECT " + info + " FROM weapon WHERE " + table + "." + info + " =  \'" + name + "\'";
+
+        m_val = getTableValue(strSelect);
+
+        if (table == "weapon")
+        {
+            string strSelectMa = "SELECT " + info + " FROM material WHERE material." + info + " =  \'" + material + "\'";
+
+            m_valMaterial = getTableValue(strSelectMa);
+
+            return (m_val + m_valMaterial);
+        }
+
+        return m_val;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("avant");
-        List<Items> weaponMaterial = getMaterialWeapon();
-        Debug.Log("apres");
 
     }
 
     // Update is called once per frame
     void Update()
     {
-   
+
 
     }
+
+
 }
