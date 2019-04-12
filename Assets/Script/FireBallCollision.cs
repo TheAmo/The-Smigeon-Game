66fe5c82e82fd9bde11e2145e02ad87310aa7e5c
Taha Mohamed Lazreg <tlazreg1@gmail.com>
2019-04-12 10:28:07 -0400

    Only velocity left to fix

--- Assets/Script/FireBallCollision.cs
+++ Assets/Script/FireBallCollision.cs
@@ -7,14 +7,20 @@ public class FireBallCollision : MonoBehaviour
     public void OnCollisionEnter2D(Collision2D col)
     {
         Debug.Log("Yeet");
-        if (col.gameObject.name.Contains("Collidable"))
+        if (col.gameObject.name.Contains("Collidable") || col.gameObject.name.Contains("Player"))
         {
-            Debug.Log(col.gameObject.GetComponent<Stats>().getHitPoint());
+            Debug.Log("HP" + col.gameObject.GetComponent<Stats>().getHitPoint());
             GameObject canvas = GameObject.Find("HUDCanvas");
-            col.gameObject.GetComponent<Stats>().setHitPoint((col.gameObject.GetComponent<Stats>().getHitPoint() - 15));
-            canvas.transform.Find("SliderHealth").GetComponent<UnityEngine.UI.Slider>().value = FindObjectOfType<Player>().GetComponent<Stats>().getHitPoint();
             Debug.Log("Boom");
-            col.gameObject.GetComponent<Stats>().setHitPoint(col.gameObject.GetComponent<Stats>().getHitPoint() - 1);
+           
+            if (col.gameObject.name.Contains("Player"))
+            {
+                FindObjectOfType<Player>().ReceiveDamage(1);
+                //canvas.transform.Find("SliderHealth").GetComponent<UnityEngine.UI.Slider>().value = FindObjectOfType<Player>().GetComponent<Stats>().getHitPoint();
+            } else
+            {
+                col.gameObject.GetComponent<Stats>().setHitPoint(col.gameObject.GetComponent<Stats>().getHitPoint() - 1);
+            }
             Destroy(GameObject.FindGameObjectWithTag("Projectile"));
             //do stuff
         }
--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -84,8 +84,8 @@ public class Player : NetworkBehaviour
         isShowing = true;
         dead = false ;
 
-
-        canvas.transform.Find("SliderHealth").GetComponent<UnityEngine.UI.Slider>().maxValue = stats.getHitPoint();
+        sliderHealth = GameObject.Find("SliderHealth");
+        sliderHealth.GetComponent<UnityEngine.UI.Slider>().maxValue = stats.getHitPoint();
         canvas.transform.Find("SliderMana").GetComponent<UnityEngine.UI.Slider>().maxValue = stats.getMana();
         if (hasAuthority)
         {
@@ -111,6 +111,7 @@ public class Player : NetworkBehaviour
        
             if (!dead)
         {
+            sliderHealth.GetComponent<UnityEngine.UI.Slider>().value = stats.getHitPoint();
             if(UnityEngine.SceneManagement.SceneManager.GetSceneByName("BlacksmithShop").isLoaded == false)
             {
                 if (this.GetComponent<MoveWASD>().enabled == false)
@@ -146,7 +147,7 @@ public class Player : NetworkBehaviour
                 isShowing = !isShowing;
                 canvas.SetActive(isShowing);
             }
-            if (Input.GetKeyDown(KeyCode.K)) //TO Open Inventory. Doesn't work.
+            if (Input.GetKeyDown(KeyCode.K)) //To smack yourself in the face. Doesn't work.
             {
                 ReceiveDamage(1);
                 Debug.Log("Damage player test");
@@ -184,6 +185,7 @@ public class Player : NetworkBehaviour
     {
         stats.setHitPoint(stats.getHitPoint()-damage);
         Debug.Log("Dealt: " + damage + "   HP left: " + stats.getHitPoint());
+        //canvas.transform.Find("SliderHealth").GetComponent<UnityEngine.UI.Slider>().value = stats.getHitPoint();
         if (stats.getHitPoint() <= 0)
         {
             return (true); //Is dead
