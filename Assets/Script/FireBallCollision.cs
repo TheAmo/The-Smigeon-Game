132fee0001111379ae7fa3875b5f143044954eca
Taha Mohamed Lazreg <tlazreg1@gmail.com>
2019-04-16 10:01:25 -0400

    I did it yey, you can now yeet fireballs

--- Assets/Script/FireBallCollision.cs
+++ Assets/Script/FireBallCollision.cs
@@ -6,23 +6,33 @@ public class FireBallCollision : MonoBehaviour
 {
     public void OnCollisionEnter2D(Collision2D col)
     {
-        Debug.Log("Yeet");
-        if (col.gameObject.name.Contains("Collidable") || col.gameObject.name.Contains("Player"))
+        if (!col.gameObject.tag.Contains("Player"))
         {
-            Debug.Log("HP" + col.gameObject.GetComponent<Stats>().getHitPoint());
-            GameObject canvas = GameObject.Find("HUDCanvas");
-            Debug.Log("Boom");
-           
-            if (col.gameObject.name.Contains("Player"))
-            {
-                FindObjectOfType<Player>().ReceiveDamage(1);
-                //canvas.transform.Find("SliderHealth").GetComponent<UnityEngine.UI.Slider>().value = FindObjectOfType<Player>().GetComponent<Stats>().getHitPoint();
-            } else
-            {
-                col.gameObject.GetComponent<Stats>().setHitPoint(col.gameObject.GetComponent<Stats>().getHitPoint() - 1);
-            }
             Destroy(GameObject.FindGameObjectWithTag("Projectile"));
-            //do stuff
+        }
+        //Debug.Log("Yeet");
+        if (col.gameObject.name.Contains("Collidable") || col.gameObject.tag.Contains("Enemy"))
+        {
+            
+            
+            if (!col.gameObject.name.Contains("Collidable"))
+            {
+                Debug.Log("HP" + col.gameObject.GetComponent<Stats>().getHitPoint());
+                GameObject canvas = GameObject.Find("HUDCanvas");
+                Debug.Log("Boom");
+
+                if (col.gameObject.name.Contains("Player"))
+                {
+                    FindObjectOfType<Player>().ReceiveDamage(1);
+                    //canvas.transform.Find("SliderHealth").GetComponent<UnityEngine.UI.Slider>().value = FindObjectOfType<Player>().GetComponent<Stats>().getHitPoint();
+                }
+                else
+                {
+                    col.gameObject.GetComponent<Stats>().setHitPoint(col.gameObject.GetComponent<Stats>().getHitPoint() - 1);
+                }
+            }
+           
+
         }
     }
 }
--- Assets/Script/Player/Controls/PlayerFireball.cs
+++ Assets/Script/Player/Controls/PlayerFireball.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class PlayerFireball : MonoBehaviour
 {
+    public GameObject fb2;
     public Sprite fbSprite;
+    static private float Pi = 3.141592653589f;
     private bool fireActivated = false;
     private GameObject canvas;
-    GameObject fb;
+    private GameObject fb;
+    private int cost = 10;
 
     // Jo helped with float :)
+
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Q) && fireActivated == false && this.GetComponent<Stats>().getMana() >= 15)
-        {
-            //fireActivated = true;
+        if (Input.GetKeyUp(KeyCode.Q) && fireActivated == false && this.GetComponent<Stats>().getMana() >= cost)
+        { 
             canvas = GameObject.Find("HUDCanvas");
-            this.GetComponent<Stats>().setMana((this.GetComponent<Stats>().getMana() - 15));
+            this.GetComponent<Stats>().setMana((this.GetComponent<Stats>().getMana() - cost));
             canvas.transform.Find("SliderMana").GetComponent<UnityEngine.UI.Slider>().value = this.GetComponent<Stats>().getMana();
             fb = new GameObject();
             fb.name = "Fireball";
@@ -25,6 +28,8 @@ public class PlayerFireball : MonoBehaviour
             fb.GetComponent<SpriteRenderer>().sortingOrder = 2;
             fb.AddComponent<Rigidbody2D>().gravityScale = 0;
             fb.GetComponent<Rigidbody2D>().MoveRotation(this.GetComponent<Rigidbody2D>().rotation);
+            Debug.Log(this.GetComponent<Rigidbody2D>().rotation);
+            fb.GetComponent<Rigidbody2D>().velocity = new Vector2(10 * Mathf.Cos(fb.GetComponent<Rigidbody2D>().rotation), 10 * Mathf.Sin(fb.GetComponent<Rigidbody2D>().rotation));
             fb.AddComponent<BoxCollider2D>().size = new Vector2((float)2.8, (float)4.7);
             fb.GetComponent<Rigidbody2D>().angularDrag = 25000;
             fb.GetComponent<Rigidbody2D>().drag = 0;
@@ -34,9 +39,9 @@ public class PlayerFireball : MonoBehaviour
             fb.transform.position = v2;
            
           
-            Debug.Log(this.GetComponent<Rigidbody2D>().rotation);
+            Debug.Log(((this.transform.rotation.z) * 180) / Pi);
 
-            fb.GetComponent<Rigidbody2D>().velocity = new Vector2(10*Mathf.Cos(this.GetComponent<Rigidbody2D>().rotation+270), 10*Mathf.Sin(this.GetComponent<Rigidbody2D>().rotation+270));
+            fb.GetComponent<Rigidbody2D>().velocity = (this.transform.rotation) * new Vector2(0, 100);
         }
     }
 
