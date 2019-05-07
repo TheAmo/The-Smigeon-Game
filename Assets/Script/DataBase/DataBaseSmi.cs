77df485503887e728554156e1a7b7ba983af6b13
Jacty <jactysaenz@gmail.com>
2019-05-06 21:45:08 -0400

    Clean DB script

--- Assets/Script/DataBase/DataBaseSmi.cs
+++ Assets/Script/DataBase/DataBaseSmi.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 using UnityNpgsql;
 using UnityNpgsqlTypes;
 
+
 public class DataBaseSmi : MonoBehaviour
 {
     /*===================================================================================================================
@@ -55,17 +56,34 @@ public class DataBaseSmi : MonoBehaviour
     }
 
     /*===================================================================================================================
-     * Get Monster Stat by Id
+     * Get Monster Stats List by id
      * 
     ===================================================================================================================*/
-    public DataTable getMonsterInfoById(int id)
+    public List<MonsterStats> getMonster(int id)
     {
+        List<MonsterStats> monster = new List<MonsterStats>();
+        string name;
+        int xp, hitpoint, armorclass, attackbonus, damagebonus, damagedice;
+
+        string strSelect = "SELECT name, xp, hitpoint, armorclass, attackbonus, damagebonus, damagedice FROM monster_stats WHERE id = " + id;
+
         m_dbTable = new DataTable();
-        m_dbTable = Connection("SELECT * FROM monster_stats WHERE id=" + id);
+        m_dbTable = Select(strSelect);
 
-        return m_dbTable;
+        for (int i = 0; i < m_dbTable.Rows.Count; i++)
+        {
+            name = (m_dbTable.Rows[i]["name"]).ToString();
+            xp = Convert.ToInt16(m_dbTable.Rows[i]["xp"]);
+            hitpoint = Convert.ToInt16(m_dbTable.Rows[i]["hitpoint"]);
+            armorclass = Convert.ToInt16(m_dbTable.Rows[i]["armorclass"]);
+            attackbonus = Convert.ToInt16(m_dbTable.Rows[i]["attackbonus"]);
+            damagebonus = Convert.ToInt16(m_dbTable.Rows[i]["damagebonus"]);
+            damagedice = Convert.ToInt16(m_dbTable.Rows[i]["damagedice"]);
+
+            monster.Add(new MonsterStats(id, name, xp, hitpoint, armorclass, attackbonus, damagebonus, damagedice));
+        }
+        return monster;
     }
-
     /*===================================================================================================================
     * Get all materials
     * Select Material
@@ -126,7 +144,7 @@ public class DataBaseSmi : MonoBehaviour
         return armor;
     }
     /*===================================================================================================================
-    * Select player entry
+    * Select player entry 
     * 
     ===================================================================================================================*/
     public List<PlayerEntryDB> getPlayerEntry()
@@ -136,7 +154,7 @@ public class DataBaseSmi : MonoBehaviour
         int experience, gold;
         var position = new float[2];
 
-        string strSelect = "SELECT name, class_name, positionx, positiony, gold FROM \"player_entry\"";
+        string strSelect = "SELECT name, class_name, positionx, positiony, gold FROM player_entry";
 
         m_dbTable = new DataTable();
         m_dbTable = Select(strSelect);
@@ -156,7 +174,7 @@ public class DataBaseSmi : MonoBehaviour
         return playerList;
     }
     /*===================================================================================================================
-    * Get Name 
+    * Get Name  by id
     * Get material name or armor name
     ===================================================================================================================*/
     public string getItemName(int id, string table)
@@ -174,7 +192,7 @@ public class DataBaseSmi : MonoBehaviour
         return name;
     }
     /*===================================================================================================================
-    * Get Class name 
+    * Get Class name by id
     * 
     ===================================================================================================================*/
     public string getClassName(string table, int id)
@@ -189,7 +207,7 @@ public class DataBaseSmi : MonoBehaviour
         return class_name;
     }
     /*===================================================================================================================
-    * Get position x and position y
+    * Get position x and position y by id
     * 
     ===================================================================================================================*/
     public int getPosition(string table, int id, string pos)
@@ -204,7 +222,7 @@ public class DataBaseSmi : MonoBehaviour
         return position;
     }
     /*===================================================================================================================
-    * Get experience
+    * Get experience by id
     * 
     ===================================================================================================================*/
     public int getExperience(string table, int id)
@@ -219,7 +237,7 @@ public class DataBaseSmi : MonoBehaviour
         return experience;
     }
     /*===================================================================================================================
-    * Get gold
+    * Get gold by id
     * 
     ===================================================================================================================*/
     public int getGold(string table, int id)
@@ -234,7 +252,7 @@ public class DataBaseSmi : MonoBehaviour
         return gold;
     }
     /*===================================================================================================================
-    * Get damage
+    * Get material damage by id
     * 
     ===================================================================================================================*/
     public int getMaterialDamage(int id)
@@ -246,12 +264,10 @@ public class DataBaseSmi : MonoBehaviour
         m_dbTable = Select(strSelect);
         int damage = Convert.ToInt16(m_dbTable.Rows[0]["damage"]);
 
-        //Debug.Log();
-
         return damage;
     }
     /*===================================================================================================================
-    * Get defense
+    * Get armor defense by id
     * 
     ===================================================================================================================*/
     public int getArmorDefense(int id)
@@ -264,7 +280,7 @@ public class DataBaseSmi : MonoBehaviour
         return defense;
     }
     /*===================================================================================================================
-    * Get price
+    * Get price by id
     * 
     ===================================================================================================================*/
     public int getPrice(int id, string table)
@@ -305,26 +321,94 @@ public class DataBaseSmi : MonoBehaviour
         dbConnection.Close();
     }
 
-    public void SavePlayer(string name, int experience, string weapon, string armor, float[] position)
+    public void UpdatePlayerInfo(string infoToSave, int id, string newValue)
     {
+        string strQuery = "UPDATE save_player SET " + infoToSave + " = :infoToSave WHERE id = :ID";
+        dbConnection = new NpgsqlConnection(strConnection);
+
+        dbConnection.Open();
+
+        dbCmd = new NpgsqlCommand(strQuery, dbConnection);
+        dbCmd.Parameters.Add(new NpgsqlParameter("infoToSave", NpgsqlDbType.Text));
+        dbCmd.Parameters.Add(new NpgsqlParameter("ID", NpgsqlDbType.Integer));
+        dbCmd.Parameters[0].Value = newValue;
+        dbCmd.Parameters[1].Value = id;
+        dbCmd.ExecuteNonQuery();
+
+        Debug.Log("UpdateWeapon");
+
+        dbConnection.Close();
+    }
+
+    public List<PlayerEntryDB> getPlayerByName(string name)
+    {
+        List<PlayerEntryDB> playerList = new List<PlayerEntryDB>();
+
+        int experience, gold;
+        string weapon, armor;
+        var position = new float[2];
+
+        string strSelect = "SELECT experience, weapon, armor, positionx, positiony, gold FROM player_entry WHERE name = " + name;
+
+        m_dbTable = new DataTable();
+        m_dbTable = Select(strSelect);
 
 
+        experience = Convert.ToInt32(m_dbTable.Rows[0]["experience"]);
+        weapon = (m_dbTable.Rows[1]["weapon"]).ToString();
+        armor = (m_dbTable.Rows[1]["armor"]).ToString();
+        position[0] = Convert.ToInt32(m_dbTable.Rows[2]["positionx"]);
+        position[1] = Convert.ToInt32(m_dbTable.Rows[3]["positiony"]);
+        gold = Convert.ToInt32(m_dbTable.Rows[4]["gold"]);
+
+        playerList.Add(new PlayerEntryDB(name, experience, weapon, armor, position, gold));
+
+        return playerList;
+    }
+
+    public void SaveNewPlayer(string name, int experience, string weapon, string armor, float[] position, int gold)
+    {
+        NpgsqlDataAdapter dbAdapter = new NpgsqlDataAdapter();
+        NpgsqlParameter dbParam = new NpgsqlParameter();
+        dbConnection = new NpgsqlConnection(strConnection);
+
+        string strQuery = "INSERT INTO save_player (name, experience, weapon, armor, positionx, positiony, gold) VALUES(@name, @experience, @weapon, @armor, @positionx, @positiony, @gold)";
+
+        dbConnection.Open();
+
+        dbCmd = new NpgsqlCommand(strQuery, dbConnection);
+
+        dbCmd.Parameters.Add(new NpgsqlParameter("@name", NpgsqlDbType.Text));
+        dbCmd.Parameters.Add(new NpgsqlParameter("@experience", NpgsqlDbType.Integer));
+        dbCmd.Parameters.Add(new NpgsqlParameter("@weapon", NpgsqlDbType.Text));
+        dbCmd.Parameters.Add(new NpgsqlParameter("@armor", NpgsqlDbType.Text));
+        dbCmd.Parameters.Add(new NpgsqlParameter("@positionx", NpgsqlDbType.Integer));
+        dbCmd.Parameters.Add(new NpgsqlParameter("@positiony", NpgsqlDbType.Integer));
+        dbCmd.Parameters.Add(new NpgsqlParameter("@gold", NpgsqlDbType.Integer));
 
 
+        dbCmd.Parameters[0].Value = name;
+        dbCmd.Parameters[1].Value = experience;
+        dbCmd.Parameters[2].Value = weapon;
+        dbCmd.Parameters[3].Value = armor;
+        dbCmd.Parameters[4].Value = position[0];
+        dbCmd.Parameters[5].Value = position[1];
+        dbCmd.Parameters[6].Value = gold;
+
+        dbCmd.ExecuteNonQuery();
+        Debug.Log("Player saved");
+        dbConnection.Close();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        List<Items> armor = getAllArmors();
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-
     }
-
-
 }
--- Assets/Script/DataBase/db.sql
+++ Assets/Script/DataBase/db.sql
@@ -16,18 +16,6 @@ INSERT INTO material (name, price, damage) VALUES ('adamant', 1250, 5);
 INSERT INTO material (name, price, damage) VALUES ('rune', 3000, 6);
 INSERT INTO material (name, price, damage) VALUES ('draconic', 10000, 7);
 
---SHOW
-SELECT name, price, damage FROM material;
-
---PLAYER INVENTORY TABLE
---DROP TABLE IF EXISTS player_inventory CASCADE;
---CREATE TABLE player_inventory(
-	--id serial PRIMARY KEY,
-	--weapons varchar(255) NOT NULL,
-	--money int NOT NULL,
-	--items varchar(255) NOT NULL	
---);
-
 --MONSTER STATS TABLE 
 DROP TABLE IF EXISTS monster_stats CASCADE;
 CREATE TABLE monster_stats(
@@ -42,13 +30,41 @@ CREATE TABLE monster_stats(
 );
 
 -- INSERT INFO INTO MONSTER STATS TABLE 
-INSERT INTO monster_stats (name, xp, hitpoint, armorclass, attackbonus, 
-						   damagebonus, damagedice) 
+INSERT INTO monster_stats (name, xp, hitpoint, armorclass, attackbonus, damagebonus, damagedice) 
 VALUES ('Goblin', 50, 7, 15, 4, 2, 6);
 
--- SHOW 
-SELECT name, xp, hitpoint, armorclass, attackbonus,damagebonus, damagedice
-FROM monster_stats;
+INSERT INTO monster_stats (name, xp, hitpoint, armorclass, attackbonus, damagebonus, damagedice) 
+VALUES ('Helmed Horror',1100, 60, 20, 6, 4, 8);
+
+INSERT INTO monster_stats (name, xp, hitpoint, armorclass, attackbonus, damagebonus, damagedice) 
+VALUES ('Pegasus',450, 59, 12, 6, 4, 6);
+
+INSERT INTO monster_stats (name, xp, hitpoint, armorclass, attackbonus, damagebonus, damagedice) 
+VALUES ('Kraken',50000, 472, 18, 18, 10, 8);
+
+INSERT INTO monster_stats (name, xp, hitpoint, armorclass, attackbonus, damagebonus, damagedice) 
+VALUES ('Unicorn',1800, 67, 12, 7, 4, 8);
+
+INSERT INTO monster_stats (name, xp, hitpoint, armorclass, attackbonus, damagebonus, damagedice) 
+VALUES ('Pixie',50, 1, 15, 7, 5, 4);
+
+INSERT INTO monster_stats (name, xp, hitpoint, armorclass, attackbonus, damagebonus, damagedice) 
+VALUES ('Deva',5900, 136, 17, 8, 4, 6);
+
+INSERT INTO monster_stats (name, xp, hitpoint, armorclass, attackbonus, damagebonus, damagedice) 
+VALUES ('Griffon',450, 59, 12, 6, 4, 8);
+
+INSERT INTO monster_stats (name, xp, hitpoint, armorclass, attackbonus, damagebonus, damagedice) 
+VALUES ('Water Weird',700, 58, 13, 5, 6, 3);
+
+INSERT INTO monster_stats (name, xp, hitpoint, armorclass, attackbonus, damagebonus, damagedice) 
+VALUES ('Coualt',1100, 341, 20, 8, 5, 6);
+
+INSERT INTO monster_stats (name, xp, hitpoint, armorclass, attackbonus, damagebonus, damagedice) 
+VALUES ('Dragon',18000, 59, 12, 12, 7, 12);
+
+INSERT INTO monster_stats (name, xp, hitpoint, armorclass, attackbonus, damagebonus, damagedice) 
+VALUES ('Death Knight',18000, 180, 20, 11, 5, 8);
 
 --PLAYER ENTRY TABLE
 DROP TABLE IF EXISTS player_entry CASCADE;
@@ -61,12 +77,10 @@ CREATE TABLE player_entry(
 	positiony float NOT NULL,
 	gold int NOT NULL	
 );
-INSERT INTO player_entry (name, class_name, experience, positionx, positiony, gold) 
-VALUES ('allo', 'allo2', 3, 0, 1, 4);
 
--- SHOW 
-SELECT name, xp, hitpoint, armorclass, attackbonus,damagebonus, damagedice
-FROM monster_stats;
+-- INSERT INFO INTO PLAYER_ENTRY TABLE 
+INSERT INTO player_entry (name, class_name, experience, positionx, positiony, gold) 
+VALUES ('new player', 'allo2', 3, 0, 1, 4);
 
 -- CLASS PLAYER TABLE
 DROP TABLE IF EXISTS class_player CASCADE;
@@ -88,17 +102,23 @@ CREATE TABLE armor(
 	defense int NOT NULL
 );
 
+-- INSERT INFO INTO ARMOR TABLE
 INSERT INTO armor (name, price, defense) VALUES ('armor1', 0, 0.15);
 INSERT INTO armor (name, price, defense) VALUES ('armor2', 500, 0.3);
 INSERT INTO armor (name, price, defense) VALUES ('armor3', 2500, 0.45);
 INSERT INTO armor (name, price, defense) VALUES ('armor4', 5000, 0.60);
 INSERT INTO armor (name, price, defense) VALUES ('armor5', 10000, 0.75);
-SELECT * FROM material;
 
-
-
-
-
-
-
-			
+-- SAVE PLAYER TABLE
+DROP TABLE IF EXISTS save_player CASCADE;
+CREATE TABLE save_player(
+	id serial PRIMARY KEY,
+	name varchar(255) NOT NULL,
+	experience int NOT NULL,
+	weapon varchar(255) NOT NULL,
+	armor varchar(255) NOT NULL,
+	positionx int NOT NULL,
+	positiony int NOT NULL, 
+	gold int NOT NULL
+);
+INSERT INTO save_player (name, experience, weapon, armor, positionx, positiony, gold) VALUES ('new', 1, 'bronze', 'bronze', 1,2, 100);
